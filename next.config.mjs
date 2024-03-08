6186c02ca69cb9b452b04a1eb47a173fcabf4267
canto1380 <atapenalba16@gmail.com>
2024-03-08 19:55:49 -0300

    configuracion babel

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
     REACT_APP_API: 'http://localhost:4000',
+  },
+  babel: {
+    presets: ['@babel/preset-env', '@babel/preset-react'],
+    // Add any additional Babel configurations here
   },
 }
 
