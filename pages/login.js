a72d13764ea004ded7896c8142b938481f276eed
canto1380 <atapenalba16@gmail.com>
2024-03-08 21:18:37 -0300

    probando correcciones errores webpack 2

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,6 @@
 import { useContext, useEffect } from 'react'
 import { User } from '../context/userProvider'
-import LoginContainer from '../containers/Login/Login.js'
+import Login from '../containers/Login/Login.js'
 import { validaToken } from '../utils/validations/validaciones'
 
 const Login = () => {
