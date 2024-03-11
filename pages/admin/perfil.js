01aefb9813aa56b1cc2d6c47a456396ac5f391d7
canto1380 <atapenalba16@gmail.com>
2024-03-10 22:37:49 -0300

    prueba 1

--- pages/admin/perfil.js
+++ pages/admin/perfil.js
@@ -27,27 +27,27 @@ const Perfil = ({ userInfo }) => {
   )
 }
 
-export const getServerSideProps = async (ctx) => {
-  const tokenCOOKIES = getTokenCOOKIES(ctx)
-  const idCOOKIES = getUserCOOKIES(ctx)
+// export const getServerSideProps = async (ctx) => {
+//   const tokenCOOKIES = getTokenCOOKIES(ctx)
+//   const idCOOKIES = getUserCOOKIES(ctx)
 
-  const params = { search: idCOOKIES }
-  const res = await getUsuarios(params, tokenCOOKIES)
-  if (res) {
-    const { rows } = res
-    return {
-      props: {
-        userInfo: rows[0],
-      },
-    }
-  } else {
-    return {
-      redirect: {
-        destination: '/login',
-        permanent: false,
-      },
-    }
-  }
-}
+//   const params = { search: idCOOKIES }
+//   const res = await getUsuarios(params, tokenCOOKIES)
+//   if (res) {
+//     const { rows } = res
+//     return {
+//       props: {
+//         userInfo: rows[0],
+//       },
+//     }
+//   } else {
+//     return {
+//       redirect: {
+//         destination: '/login',
+//         permanent: false,
+//       },
+//     }
+//   }
+// }
 
 export default Perfil
