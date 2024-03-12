3cf27821ccc2217362af10d7ed4a1045a9035984
canto1380 <atapenalba16@gmail.com>
2024-03-12 17:23:28 -0300

    prueba 8

--- utils/queryAPI/usuarios.js
+++ utils/queryAPI/usuarios.js
@@ -2,6 +2,8 @@ import { apiParams } from './api'
 
 export const getUsuarios = async (params, token, usuario, role) => {
   const res = await apiParams('GET', params, `usuarios`, '', token)
+  console.log(`QUERY:  ${res}`)
+  return data
   if (res.status === 200) {
     const data = res?.data
     return data
